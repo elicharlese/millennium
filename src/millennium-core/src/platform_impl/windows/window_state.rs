ad7a345e23858c4f596c307674133334ac949626
Carson M <61946759+sudo-carson@users.noreply.github.com>
2022-08-19 16:03:52 -0500

    fix(core): focus events being sent to inactive windows
    Upstream from tao: 23ae71b

--- src/millennium-core/src/platform_impl/windows/window_state.rs
+++ src/millennium-core/src/platform_impl/windows/window_state.rs
@@ -56,7 +56,10 @@ pub struct WindowState {
 
 	pub ime_handler: MinimalIme,
 
-	pub window_flags: WindowFlags
+	pub window_flags: WindowFlags,
+
+	pub is_active: bool,
+	pub is_focused: bool
 }
 
 #[derive(Clone)]
@@ -147,7 +150,9 @@ impl WindowState {
 			preferred_theme,
 			high_surrogate: None,
 			ime_handler: MinimalIme::default(),
-			window_flags: WindowFlags::empty()
+			window_flags: WindowFlags::empty(),
+			is_active: false,
+			is_focused: false
 		}
 	}
 
@@ -172,6 +177,24 @@ impl WindowState {
 		F: FnOnce(&mut WindowFlags)
 	{
 		f(&mut self.window_flags);
+	}
+
+	pub fn has_active_focus(&self) -> bool {
+		self.is_active && self.is_focused
+	}
+
+	// Updates `is_active` and returns whether the active focus state has changed.
+	pub fn set_active(&mut self, is_active: bool) -> bool {
+		let old = self.has_active_focus();
+		self.is_active = is_active;
+		old != self.has_active_focus()
+	}
+
+	// Updates `is_focused` and returns whether the active focus state has changed.
+	pub fn set_focused(&mut self, is_focused: bool) -> bool {
+		let old = self.has_active_focus();
+		self.is_focused = is_focused;
+		old != self.has_active_focus()
 	}
 }
 
