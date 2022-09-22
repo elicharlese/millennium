d268217814f3b1a5dbb2873f9f4f85992a9a7e0b
Carson M <61946759+sudo-carson@users.noreply.github.com>
2022-09-22 13:11:37 -0500

    fix(core): fix `is_decorated` returning wrong value on macOS
    Upstream from tao: c3e076e

--- src/millennium-core/src/platform_impl/macos/window.rs
+++ src/millennium-core/src/platform_impl/macos/window.rs
@@ -794,11 +794,7 @@ impl UnownedWindow {
 
 	#[inline]
 	pub fn is_decorated(&self) -> bool {
-		let current_mask = unsafe { self.ns_window.styleMask() };
-		if current_mask == NSWindowStyleMask::NSMiniaturizableWindowMask | NSWindowStyleMask::NSResizableWindowMask {
-			return false;
-		}
-		true
+		self.decorations.load(Ordering::Acquire)
 	}
 
 	#[inline]
