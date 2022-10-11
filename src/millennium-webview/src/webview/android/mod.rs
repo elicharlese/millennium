33dcab4e9c9bef585a2ac68b7801068ad1e3d13a
Carson M <61946759+sudo-carson@users.noreply.github.com>
2022-10-11 15:56:21 -0500

    fix(webview): retrieve custom protocol schemes on android
    Upstream from tao: 375c908

--- src/millennium-webview/src/webview/android/mod.rs
+++ src/millennium-webview/src/webview/android/mod.rs
@@ -36,8 +36,7 @@ impl InnerWebView {
 		if let Some(u) = url {
 			let mut url_string = String::from(u.as_str());
 			let name = u.scheme();
-			// TODO: Expands custom protocols with real configurations
-			let schemes = vec!["assets", "res"];
+			let schemes = attributes.custom_protocols.iter().map(|(name, _)| name.as_str()).collect::<Vec<_>>();
 			if schemes.contains(&name) {
 				url_string = u.as_str().replace(&format!("{}://", name), "https://millennium.pyke.io/")
 			}
