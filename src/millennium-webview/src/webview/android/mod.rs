c1c5779a85025e55a8fa213fe5628b0f90f73529
Carson M <61946759+sudo-carson@users.noreply.github.com>
2022-10-11 16:09:37 -0500

    feat(webview): reimpl android logic
    Upstream from wry: aba1ae5

--- src/millennium-webview/src/webview/android/mod.rs
+++ src/millennium-webview/src/webview/android/mod.rs
@@ -13,15 +13,85 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 
-use std::{rc::Rc, str::FromStr};
+use std::rc::Rc;
 
-use millennium_core::android::ndk_glue::*;
+use millennium_core::platform::android::ndk_glue::{
+	jni::{objects::GlobalRef, JNIEnv},
+	ndk::looper::{FdEvent, ForeignLooper}
+};
+use once_cell::sync::OnceCell;
 
 use crate::{
 	application::window::Window,
-	http::{method::Method, Request as HttpRequest, RequestParts},
+	http::{Request as HttpRequest, Response as HttpResponse},
 	Result
 };
+
+pub(crate) mod binding;
+mod main_pipe;
+use self::main_pipe::{MainPipe, WebViewMessage, MAIN_PIPE};
+
+#[macro_export]
+macro_rules! android_binding {
+	($domain:ident, $package:ident, $main: ident) => {
+		android_binding!($domain, $package, $main, ::millennium_webview)
+	};
+	($domain:ident, $package:ident, $main: ident, $millennium_webview: path) => {
+		use $millennium_webview::{
+			application::{android_binding as core_android_binding, android_fn, platform::android::ndk_glue::*},
+			webview::prelude::*
+		};
+		core_android_binding!($domain, $package, setup, $main);
+		android_fn!($domain, $package, RustWebChromeClient, runInitializationScripts);
+		android_fn!($domain, $package, RustWebViewClient, handleRequest, JObject, jobject);
+		android_fn!($domain, $package, Ipc, ipc, JString);
+	};
+}
+
+pub static IPC: OnceCell<UnsafeIpc> = OnceCell::new();
+pub static REQUEST_HANDLER: OnceCell<UnsafeRequestHandler> = OnceCell::new();
+
+pub struct UnsafeIpc(Box<dyn Fn(&Window, String)>, Rc<Window>);
+impl UnsafeIpc {
+	pub fn new(f: Box<dyn Fn(&Window, String)>, w: Rc<Window>) -> Self {
+		Self(f, w)
+	}
+}
+unsafe impl Send for UnsafeIpc {}
+unsafe impl Sync for UnsafeIpc {}
+
+pub struct UnsafeRequestHandler(Box<dyn Fn(HttpRequest) -> Option<HttpResponse>>);
+impl UnsafeRequestHandler {
+	pub fn new(f: Box<dyn Fn(HttpRequest) -> Option<HttpResponse>>) -> Self {
+		Self(f)
+	}
+}
+unsafe impl Send for UnsafeRequestHandler {}
+unsafe impl Sync for UnsafeRequestHandler {}
+
+pub unsafe fn setup(env: JNIEnv, looper: &ForeignLooper, activity: GlobalRef) {
+	let mut main_pipe = MainPipe {
+		env,
+		activity,
+		initialization_scripts: vec![],
+		webview: None
+	};
+
+	looper
+		.add_fd_with_callback(MAIN_PIPE[0], FdEvent::INPUT, move |_| {
+			let size = std::mem::size_of::<bool>();
+			let mut wake = false;
+			if libc::read(MAIN_PIPE[0], &mut wake as *mut _ as *mut _, size) == size as libc::ssize_t {
+				match main_pipe.recv() {
+					Ok(_) => true,
+					Err(_) => false
+				}
+			} else {
+				false
+			}
+		})
+		.unwrap();
+}
 
 pub struct InnerWebView {
 	pub window: Rc<Window>
@@ -50,31 +120,17 @@ impl InnerWebView {
 		}
 
 		REQUEST_HANDLER.get_or_init(move || {
-			UnsafeRequestHandler::new(Box::new(move |request: WebResourceRequest| {
+			UnsafeRequestHandler::new(Box::new(move |mut request| {
 				if let Some(custom_protocol) = custom_protocols
 					.iter()
-					.find(|(name, _)| request.url.starts_with(&format!("https://{}.", name)))
+					.find(|(name, _)| request.uri().starts_with(&format!("https://{}.", name)))
 				{
-					let path = request
-						.url
+					*request.uri_mut() = request
+						.uri()
 						.replace(&format!("https://{}.", custom_protocol.0), &format!("{}://", custom_protocol.0));
 
-					let request = HttpRequest {
-						head: RequestParts {
-							method: Method::from_str(&request.method).unwrap_or(Method::GET),
-							uri: path,
-							headers: request.headers
-						},
-						body: Vec::new()
-					};
-
 					if let Ok(response) = (custom_protocol.1)(&request) {
-						return Some(WebResourceResponse {
-							status: response.head.status,
-							headers: response.head.headers,
-							mimetype: response.head.mimetype,
-							body: response.body
-						});
+						return Some(response);
 					}
 				}
 
@@ -84,7 +140,7 @@ impl InnerWebView {
 
 		let w = window.clone();
 		if let Some(i) = ipc_handler {
-			IPC.get_or_init(move || UnsafeIpc::new(Box::into_raw(Box::new(i)) as *mut _, w));
+			IPC.get_or_init(move || UnsafeIpc::new(Box::new(i), w));
 		}
 
 		Ok(Self { window })
--- src/millennium-webview/src/webview/mod.rs
+++ src/millennium-webview/src/webview/mod.rs
@@ -23,6 +23,10 @@ pub use web_context::WebContext;
 #[cfg(target_os = "android")]
 pub(crate) mod android;
 #[cfg(target_os = "android")]
+pub mod prelude {
+	pub use super::android::{binding::*, setup};
+}
+#[cfg(target_os = "android")]
 use android::*;
 #[cfg(any(target_os = "linux", target_os = "dragonfly", target_os = "freebsd", target_os = "netbsd", target_os = "openbsd"))]
 pub(crate) mod webkitgtk;
