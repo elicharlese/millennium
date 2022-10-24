bc6123f016e50c2dc0413a3fad0bb98ccfbaecbb
Carson M <61946759+sudo-carson@users.noreply.github.com>
2022-10-24 14:56:18 -0500

    refactor(webview): make android env vars optional in build script
    Upstream from wry: 3624414

--- src/millennium-webview/build.rs
+++ src/millennium-webview/build.rs
@@ -25,39 +25,40 @@ fn main() {
 		use std::{fs, path::PathBuf};
 
 		fn env_var(var: &str) -> String {
-			std::env::var(var).unwrap_or_else(|_| panic!(" `{}` is not set, which is needed to generate the kotlin files for android.", var))
+			std::env::var(var).unwrap_or_else(|_| panic!("`{}` is not set, which is required to generate Kotlin files for Android.", var))
 		}
 
 		println!("cargo:rerun-if-env-changed=MILLENNIUM_ANDROID_REVERSED_DOMAIN");
 		println!("cargo:rerun-if-env-changed=MILLENNIUM_ANDROID_APP_NAME_SNAKE_CASE");
 		println!("cargo:rerun-if-env-changed=MILLENNIUM_ANDROID_KOTLIN_FILES_OUT_DIR");
 
-		let reversed_domain = env_var("MILLENNIUM_ANDROID_REVERSED_DOMAIN");
-		let app_name_snake_case = env_var("MILLENNIUM_ANDROID_APP_NAME_SNAKE_CASE");
-		let kotlin_out_dir = env_var("MILLENNIUM_ANDROID_KOTLIN_FILES_OUT_DIR");
+		if let Ok(kotlin_out_dir) = std::env::var("MILLENNIUM_ANDROID_KOTLIN_FILES_OUT_DIR") {
+			let reversed_domain = env_var("MILLENNIUM_ANDROID_REVERSED_DOMAIN");
+			let app_name_snake_case = env_var("MILLENNIUM_ANDROID_APP_NAME_SNAKE_CASE");
 
-		let kotlin_out_dir = PathBuf::from(kotlin_out_dir).canonicalize().expect("Failed to canonicalize path");
-		let kotlin_files_path = PathBuf::from(env_var("CARGO_MANIFEST_DIR")).join("src/webview/android/kotlin");
-		println!("cargo:rerun-if-changed={}", kotlin_files_path.display());
-		let kotlin_files = fs::read_dir(kotlin_files_path).expect("failed to read kotlin directory");
+			let kotlin_out_dir = PathBuf::from(kotlin_out_dir).canonicalize().expect("Failed to canonicalize path");
+			let kotlin_files_path = PathBuf::from(env_var("CARGO_MANIFEST_DIR")).join("src/webview/android/kotlin");
+			println!("cargo:rerun-if-changed={}", kotlin_files_path.display());
+			let kotlin_files = fs::read_dir(kotlin_files_path).expect("failed to read kotlin directory");
 
-		for file in kotlin_files {
-			let file = file.unwrap();
+			for file in kotlin_files {
+				let file = file.unwrap();
 
-			let class_extension_env = format!("MILLENNIUM_{}_CLASS_EXTENSION", file.path().file_stem().unwrap().to_string_lossy().to_uppercase());
-			let class_init_env = format!("MILLENNIUM_{}_CLASS_INIT", file.path().file_stem().unwrap().to_string_lossy().to_uppercase());
+				let class_extension_env = format!("MILLENNIUM_{}_CLASS_EXTENSION", file.path().file_stem().unwrap().to_string_lossy().to_uppercase());
+				let class_init_env = format!("MILLENNIUM_{}_CLASS_INIT", file.path().file_stem().unwrap().to_string_lossy().to_uppercase());
 
-			println!("cargo:rerun-if-env-changed={}", class_extension_env);
-			println!("cargo:rerun-if-env-changed={}", class_init_env);
+				println!("cargo:rerun-if-env-changed={}", class_extension_env);
+				println!("cargo:rerun-if-env-changed={}", class_init_env);
 
-			let content = fs::read_to_string(file.path())
-				.expect("failed to read kotlin file as string")
-				.replace("{{app-domain-reversed}}", &reversed_domain)
-				.replace("{{app-name-snake-case}}", &app_name_snake_case)
-				.replace("{{class-extension}}", &std::env::var(&class_extension_env).unwrap_or_default())
-				.replace("{{class-init}}", &std::env::var(&class_init_env).unwrap_or_default());
+				let content = fs::read_to_string(file.path())
+					.expect("failed to read kotlin file as string")
+					.replace("{{app-domain-reversed}}", &reversed_domain)
+					.replace("{{app-name-snake-case}}", &app_name_snake_case)
+					.replace("{{class-extension}}", &std::env::var(&class_extension_env).unwrap_or_default())
+					.replace("{{class-init}}", &std::env::var(&class_init_env).unwrap_or_default());
 
-			fs::write(kotlin_out_dir.join(file.file_name()), content).expect("Failed to write kotlin file");
+				fs::write(kotlin_out_dir.join(file.file_name()), content).expect("Failed to write kotlin file");
+			}
 		}
 	}
 }
