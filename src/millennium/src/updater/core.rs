487f3c9f7e471f52843bb12794d203f59fdd82ef
Carson M <carson@pyke.io>
2022-11-11 15:07:09 -0600

    fix(updater): hopefully last fix

--- src/millennium/src/updater/core.rs
+++ src/millennium/src/updater/core.rs
@@ -33,7 +33,6 @@ use std::{
 };
 
 use base64::decode;
-use futures_util::StreamExt;
 use http::{
 	header::{HeaderName, HeaderValue},
 	HeaderMap, StatusCode
@@ -535,12 +534,35 @@ impl<R: Runtime> Update<R> {
 			.and_then(|value| value.parse().ok());
 
 		let mut buffer = Vec::new();
-		let mut stream = response.bytes_stream();
-		while let Some(chunk) = stream.next().await {
-			let chunk = chunk?;
-			let bytes = chunk.as_ref().to_vec();
-			on_chunk(bytes.len(), content_length);
-			buffer.extend(bytes);
+		#[cfg(feature = "reqwest-client")]
+		{
+			use futures_util::StreamExt;
+			let mut stream = response.bytes_stream();
+			while let Some(chunk) = stream.next().await {
+				let chunk = chunk?;
+				let bytes = chunk.as_ref().to_vec();
+				on_chunk(bytes.len(), content_length);
+				buffer.extend(bytes);
+			}
+		}
+		#[cfg(not(feature = "reqwest-client"))]
+		{
+			let mut reader = response.reader();
+			let mut buf = [0; 16384];
+			loop {
+				match reader.read(&mut buf) {
+					Ok(b) => {
+						if b == 0 {
+							break;
+						} else {
+							let bytes = buf[0..b].to_vec();
+							on_chunk(bytes.len(), content_length);
+							buffer.extend(bytes);
+						}
+					}
+					Err(e) => return Err(e.into())
+				}
+			}
 		}
 
 		on_download_finish();
